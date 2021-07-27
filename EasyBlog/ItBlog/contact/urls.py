3fcdf33982fbfe1b5334b8d287a4e339ae2f252c
arthurr0104 <achanchapanyan01@gmail.com>
2021-07-27 19:48:56 +0400

    Revert "Its work"
    This reverts commit 13a9edcefa48464f76dcdaf943827607dce12ed5.

--- EasyBlog/ItBlog/contact/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CreateContactUsObject, about_us
-
-urlpatterns = [
-    path('contact_us/', CreateContactUsObject.as_view(), name='contact_us'),
-    path('about_us/', about_us, name='about_us'),
-]
